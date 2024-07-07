9759906bab86447c55920e3658af60868ea036fe
David Evans <davidje13@users.noreply.github.com>
2024-07-07 21:35:13 +0100

    Test all hooks

---
+++ spec/hooks-test-harness.js
@@ -0,0 +1,179 @@
+let CURRENT;
+let ACT_CHANGED = null;
+const POST_RENDER_LAYOUT = { down: new Set(), up: new Set() };
+const POST_RENDER_EFFECT = { down: new Set(), up: new Set() };
+
+function renderHook(hook, ...args) {
+  const state = [];
+
+  let latestArgs = args;
+  const onChange = () => ACT_CHANGED(render);
+  let first = true;
+
+  const render = () => {
+    CURRENT = { state, pos: 0, first, onChange };
+    r.current = hook(...latestArgs);
+    if (CURRENT.pos !== state.length) {
+      throw new Error('hooks changed between renders');
+    }
+    CURRENT = null;
+    first = false;
+  };
+
+  const r = {
+    current: null,
+    rerender(...args) {
+      latestArgs = args;
+      act(render);
+    },
+    unmount() {
+      act(() => {
+        for (const s of state) {
+          s.unmount?.();
+        }
+      });
+    },
+  };
+  act(render);
+  return r;
+}
+
+function act(fn) {
+  if (ACT_CHANGED !== null) {
+    throw new Error('nested act() call');
+  }
+  const changes = new Set();
+  ACT_CHANGED = (f) => changes.add(f);
+  fn();
+  const steps = [
+    () => {
+      const v = changes.values().next().value;
+      changes.delete(v);
+      v();
+    },
+    () => {
+      runEffects(POST_RENDER_LAYOUT.down, true);
+      runEffects(POST_RENDER_LAYOUT.up, false);
+    },
+    () => {
+      runEffects(POST_RENDER_EFFECT.down, true);
+      runEffects(POST_RENDER_EFFECT.up, false);
+    },
+  ];
+  for (let loopCount = 0; changes.size > 0; ++loopCount) {
+    if (loopCount > 1000) {
+      throw new Error('infinite loop');
+    }
+    for (const step of steps) {
+      step();
+      if (changes.size > 0) {
+        break;
+      }
+    }
+  }
+  ACT_CHANGED = null;
+}
+
+function runEffects(items, reverse) {
+  const fns = [...items.values()];
+  items.clear();
+  if (reverse) {
+    fns.reverse();
+  }
+  for (const fn of fns) {
+    fn();
+  }
+}
+
+function getInternalState(init) {
+  if (CURRENT.first) {
+    CURRENT.state.push(init(CURRENT.onChange));
+  }
+  return CURRENT.state[CURRENT.pos++];
+}
+
+const useRef = (initial) => getInternalState(() => ({ current: initial }));
+
+function useMemo(fn, deps) {
+  const state = getInternalState(() => ({ result: undefined, deps: undefined }));
+  if (depsChanged(state.deps, deps)) {
+    state.deps = [...deps];
+    state.result = fn();
+  }
+  return state.result;
+}
+
+function _useEffect(stage, fn, deps) {
+  const state = getInternalState(() => {
+    const state = {
+      teardown: undefined,
+      deps: undefined,
+      next: undefined,
+      up() {
+        state.teardown = state.next();
+        state.next = undefined;
+      },
+      down() {
+        state.teardown?.();
+        state.teardown = undefined;
+      },
+      unmount() {
+        stage.down.add(state.down);
+      },
+    };
+    return state;
+  });
+  if (depsChanged(state.deps, deps)) {
+    state.deps = [...deps];
+    state.next = fn;
+    stage.down.add(state.down);
+    stage.up.add(state.up);
+  }
+}
+
+const useLayoutEffect = (fn, deps) => _useEffect(POST_RENDER_LAYOUT, fn, deps);
+const useEffect = (fn, deps) => _useEffect(POST_RENDER_EFFECT, fn, deps);
+
+function useReducer(reducer, initialArg, init = (x) => x) {
+  const state = getInternalState((onChange) => {
+    const state = {
+      current: init(initialArg),
+      reducer,
+      dispatch(d) {
+        state.current = state.reducer(state.current, d);
+        onChange();
+      },
+    };
+    return state;
+  });
+  state.reducer = reducer;
+  return [state.current, state.dispatch];
+}
+
+const stateInit = (i) => (typeof i === 'function' ? i() : i);
+const stateReducer = (s, d) => (typeof d === 'function' ? d(s) : d);
+const useState = (initial) => useReducer(stateReducer, initial, stateInit);
+
+function depsChanged(a, b) {
+  if (!b || !Array.isArray(b)) {
+    throw new Error('missing deps');
+  }
+  if (!a) {
+    return true;
+  }
+  if (a.length !== b.length) {
+    throw new Error('deps changed');
+  }
+  return a.some((v, i) => b[i] !== v);
+}
+
+module.exports = {
+  renderHook,
+  act,
+  useRef,
+  useMemo,
+  useLayoutEffect,
+  useEffect,
+  useReducer,
+  useState,
+};
--- spec/hooks.test.js
+++ spec/hooks.test.js
@@ -1,5 +1,16 @@
+const {
+  renderHook,
+  act,
+  useReducer,
+  useState,
+  useMemo,
+  useRef,
+  useLayoutEffect,
+} = require('./hooks-test-harness');
 const context = require('../index');
 const { makeHooks } = require('../helpers/hooks');
+
+const allHooks = { useReducer, useState, useMemo, useRef, useLayoutEffect };
 
 describe('makeHooks', () => {
   describe('useJSONReducer', () => {
@@ -23,70 +34,90 @@ describe('makeHooks', () => {
       expect(h.current.state).same(snapshot2.state);
       expect(h.current).not(same(snapshot2)); // we did not provide useMemo
     });
+
+    it('uses useReducer directly if provided', () => {
+      const { useJSONReducer } = makeHooks(context, { useReducer });
+      const h = renderHook(useJSONReducer, { foo: 7 });
+
+      expect(h.current.state).equals({ foo: 7 });
+
+      act(() => h.current.dispatch({ foo: ['=', 8] }));
+      expect(h.current.state).equals({ foo: 8 });
+    });
+
+    it('memoises the returned structure if useMemo is provided', () => {
+      const { useJSONReducer } = makeHooks(context, { useState, useMemo });
+      const h = renderHook(useJSONReducer, { foo: 7 });
+
+      const snapshot1 = h.current;
+
+      act(() => h.current.dispatch({ foo: ['=', 8] }));
+      const snapshot2 = h.current;
+      expect(snapshot2).not(same(snapshot1));
+
+      act(() => h.current.dispatch({ foo: ['=', 8] })); // no change
+      expect(h.current).same(snapshot2);
+    });
+  });
+
+  describe('useWrappedJSONReducer', () => {
+    it('delegates to a provided storage mechanism', () => {
+      const { useWrappedJSONReducer } = makeHooks(context, allHooks);
+
+      const captured = [];
+      const h = renderHook(() => {
+        const [state, setState] = useState({ foo: 7 });
+        const setter = (v) => {
+          captured.push(v);
+          setState(v);
+        };
+        return useWrappedJSONReducer([state, setter]);
+      });
+
+      expect(captured).toHaveLength(0);
+      expect(h.current.state).equals({ foo: 7 });
+      const snapshot1 = h.current;
+
+      act(() => h.current.dispatch({ foo: ['=', 8] }));
+      expect(captured).toHaveLength(1);
+      expect(h.current.state).equals({ foo: 8 });
+
+      expect(h.current).not(same(snapshot1));
+      expect(h.current.state).not(same(snapshot1.state));
+      expect(h.current.dispatch).same(snapshot1.dispatch);
+      const snapshot2 = h.current;
+
+      act(() => h.current.dispatch({ foo: ['=', 8] })); // no change
+      expect(captured).toHaveLength(2);
+      expect(h.current.state).same(snapshot2.state);
+    });
+  });
+
+  describe('useScopedReducer', () => {
+    it('returns a reducer for a sub-section of the larger state', () => {
+      const { useJSONReducer, useScopedReducer } = makeHooks(context, allHooks);
+      const h = renderHook(() => {
+        const full = useJSONReducer({ foo: { bar: 1 } });
+        const scoped = useScopedReducer(full, ['foo']);
+        return { full, scoped };
+      });
+
+      expect(h.current.full.state).equals({ foo: { bar: 1 } });
+      expect(h.current.scoped.state).equals({ bar: 1 });
+      const scopedDispatch1 = h.current.scoped.dispatch;
+
+      act(() => h.current.scoped.dispatch({ bar: ['=', 2] }));
+      expect(h.current.full.state).equals({ foo: { bar: 2 } });
+      expect(h.current.scoped.state).equals({ bar: 2 });
+      const scopedDispatch2 = h.current.scoped.dispatch;
+
+      act(() => h.current.full.dispatch({ foo: { bar: ['=', 3] } }));
+      expect(h.current.full.state).equals({ foo: { bar: 3 } });
+      expect(h.current.scoped.state).equals({ bar: 3 });
+      const scopedDispatch3 = h.current.scoped.dispatch;
+
+      expect(scopedDispatch1).same(scopedDispatch2);
+      expect(scopedDispatch2).same(scopedDispatch3);
+    });
   });
 });
-
-let CURRENT;
-let ACT_CHANGED;
-function renderHook(hook, ...args) {
-  const state = [];
-
-  let latestArgs;
-  const onChange = () => ACT_CHANGED(rerender);
-  let first = true;
-
-  const rerender = () => {
-    CURRENT = { state, pos: 0, first, onChange };
-    r.current = hook(...latestArgs);
-    CURRENT = null;
-    first = false;
-  };
-
-  const r = {
-    current: null,
-    rerender: (...args) => {
-      latestArgs = args;
-      act(rerender);
-    },
-  };
-  r.rerender(...args);
-  return r;
-}
-
-function act(fn) {
-  const changes = new Set();
-  ACT_CHANGED = (f) => changes.add(f);
-  fn();
-  for (let n = 0; changes.size > 0; ) {
-    if (++n > 1000) {
-      throw new Error('infinite loop');
-    }
-    const v = changes.values().next().value;
-    changes.delete(v);
-    v();
-  }
-  ACT_CHANGED = null;
-}
-
-function getInternalState(init) {
-  if (CURRENT.first) {
-    const s = init(CURRENT.onChange);
-    CURRENT.state.push(s);
-    return s;
-  }
-  return CURRENT.state[CURRENT.pos++];
-}
-
-function useState(initial) {
-  const s = getInternalState((onChange) => {
-    const state = {
-      current: typeof initial === 'function' ? initial() : initial,
-      setter: (v) => {
-        state.current = typeof v === 'function' ? v(state.current) : v;
-        onChange();
-      },
-    };
-    return state;
-  });
-  return [s.current, s.setter];
-}
